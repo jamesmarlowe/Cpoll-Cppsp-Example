d9b2a8fa7e0680616c1428880742a3a64ac94245
James Marlowe <james.marlowe@lumate.com>
2015-03-30 14:53:29 -0500

    cleanup

--- install.sh
+++ install.sh
@@ -5,8 +5,8 @@ RETCODE=$(fw_exists ${IROOT}/cppsp.installed)
 
 sudo apt-get install -y libcrypto++-dev
 
-fw_get -O cppsp_0.2.3.tar.xz http://downloads.sourceforge.net/project/cpollcppsp/CPPSP%200.2%20%28testing%29/cppsp_0.2.3.tar.xz
-fw_untar cppsp_0.2.3.tar.xz
+wget -O cppsp_0.2.3.tar.xz http://downloads.sourceforge.net/project/cpollcppsp/CPPSP%200.2%20%28testing%29/cppsp_0.2.3.tar.xz
+tar xf cppsp_0.2.3.tar.xz
 
 # Using cp+rm over mv intentionally, because apparently this download
 # causes oddball issues when mv'ed around inside a folder mounted 
--- setup.sh
+++ setup.sh
@@ -4,5 +4,13 @@ export CPLUS_INCLUDE_PATH=/usr/include/zmq:$CPLUS_INCLUDE_PATH
 
 make clean
 make
-cd $CPPSP_HOME
-./run_application $TROOT/www -l 0.0.0.0:8080 -g g++-4.8 -m /forcedynamic.cppsm &
+cd /home/ubuntu/Cpoll-Cppsp-Example/cppsp_0.2.3/
+# build logging util
+#g++ -std=c++11 -g -c -Wall -fPIC log.cpp -o log.o
+g++ -std=c++11 -fpic -c log.cpp
+#ar rvs log.a log.o
+g++ -shared -o liblog.so log.o
+#/usr/lib/ld -llog -lhiredis
+sudo cp liblog.so /usr/lib/x86_64-linux-gnu
+# run the thing
+./run_application /home/ubuntu/Cpoll-Cppsp-Example/www -l 0.0.0.0:80 -g g++-4.8 -m /forcedynamic.cppsm -c -I/home/ubuntu/Cpoll-Cppsp-Example/util -c -llog -c -L/home/ubuntu/Cpoll-Cppsp-Example/util/ &
